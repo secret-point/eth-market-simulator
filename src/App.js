5a2bea091505d0726bd8e2d1c75d713a79802773
secret-point <secret.point2020@gmail.com>
2023-10-22 21:19:44 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserProvider from "./provider/user";
 import OrderProvider from "./provider/order";
-import LeaderboardProvider from './provider/leaderboard';
+import LeaderboardProvider from "./provider/leaderboard";
 import Home from "./pages/home";
-import LeaderBoard from './pages/leaderboard';
-import Profile from './pages/profile';
-import DiscordLoading from './pages/discordLoading';
-import TwitterLoading from './pages/twitterLoading';
-import './App.css';
+import LeaderBoard from "./pages/leaderboard";
+import Profile from "./pages/profile";
+import DiscordLoading from "./pages/discordLoading";
+import TwitterLoading from "./pages/twitterLoading";
+import "./App.css";
 
 function App() {
-
- 
   return (
     <UserProvider>
       <OrderProvider>
         <LeaderboardProvider>
-          <BrowserRouter >
+          <BrowserRouter>
             <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/discord-auth' element={<DiscordLoading />} />
-              <Route path='/twitter-auth' element={<TwitterLoading />} />
-              <Route path='/leaderboard' element={<LeaderBoard />} />
-              <Route path='/profile' element={<Profile />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/discord-auth" element={<DiscordLoading />} />
+              <Route path="/twitter-auth" element={<TwitterLoading />} />
+              <Route path="/leaderboard" element={<LeaderBoard />} />
+              <Route path="/profile" element={<Profile />} />
             </Routes>
           </BrowserRouter>
         </LeaderboardProvider>
--- src/components/roundInfo/index.jsx
+++ src/components/roundInfo/index.jsx
@@ -1,50 +1,60 @@
-import {useState, useContext, useEffect, useCallback} from "react";
-import {OrderContext} from "../../provider/order";
-import {UserContext} from "../../provider/user";
+import React, { useState, useContext, useEffect, useCallback } from "react";
+import { OrderContext } from "../../provider/order";
+import { UserContext } from "../../provider/user";
 
-import './index.css';
+import "./index.css";
 
 let timer;
 
 const RoundInfo = () => {
-
   const [lastSeconds, setLastSeconds] = useState(0);
   const order = useContext(OrderContext);
   const user = useContext(UserContext);
-  useEffect(()=>{
+  useEffect(() => {
     order.getRoundInfo();
-  },[])
-  
-  useEffect(()=>{
-    if(!order.roundInfo)
-      return;
+  }, []);
+
+  useEffect(() => {
+    if (!order.roundInfo) return;
     clearInterval(timer);
-    timer = setInterval(()=>{
-        const timeline = new Date(order.roundInfo.created);
-        timeline.setSeconds(timeline.getSeconds() + order.roundInfo.duration);
-        let difftime = parseInt((timeline.getTime() - Date.now())/1000);
-        const day = parseInt(difftime/(3600 * 24 ));
-        difftime = difftime%(3600 * 24 );
-        const hour = parseInt(difftime/3600);
-        difftime = difftime%3600;
-        const min = parseInt(difftime/60);
-        difftime = difftime%60;
-        const sec = difftime;
+    timer = setInterval(() => {
+      const timeline = new Date(order.roundInfo.created);
+      timeline.setSeconds(timeline.getSeconds() + order.roundInfo.duration);
+      let difftime = parseInt((timeline.getTime() - Date.now()) / 1000);
+      const day = parseInt(difftime / (3600 * 24));
+      difftime = difftime % (3600 * 24);
+      const hour = parseInt(difftime / 3600);
+      difftime = difftime % 3600;
+      const min = parseInt(difftime / 60);
+      difftime = difftime % 60;
+      const sec = difftime;
 
-        setLastSeconds(`${day}days ${hour}hours ${min}mins ${sec}seconds`);
-    }, 1000)
+      setLastSeconds(`${day}days ${hour}hours ${min}mins ${sec}seconds`);
+    }, 1000);
 
-    return(function clean(){
+    return function clean() {
       clearInterval(timer);
-    })
-  },[order.roundInfo])
+    };
+  }, [order.roundInfo]);
 
   return (
-      <div className="p4">
-        <p>The week timer: {order.roundInfo?`${lastSeconds}` : "calculating..."}</p>
-        <p>Total Balance($): {user.userInfo.islogin? (user.userInfo.usd + user.userInfo.usd_holding + user.userInfo.eth * order.ethPrice + user.userInfo.eth_holding * order.ethPrice).toFixed(2) : 0}</p>
-      </div>
+    <div className="p4">
+      <p>
+        The week timer: {order.roundInfo ? `${lastSeconds}` : "calculating..."}
+      </p>
+      <p>
+        Total Balance($):{" "}
+        {user.userInfo.islogin
+          ? (
+              user.userInfo.usd +
+              user.userInfo.usd_holding +
+              user.userInfo.eth * order.ethPrice +
+              user.userInfo.eth_holding * order.ethPrice
+            ).toFixed(2)
+          : 0}
+      </p>
+    </div>
   );
-}
+};
 
 export default RoundInfo;
